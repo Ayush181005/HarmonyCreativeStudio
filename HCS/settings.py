efb0a578346e7f37f45ecc889c6147f33f3aa928
Ayush <ayush.singh18@outlook.com>
2023-01-19 11:56:29 +0530

    Fixed issue where static files not loaded in development'

--- HCS/settings.py
+++ HCS/settings.py
@@ -133,9 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [     # Development only
+    BASE_DIR / "static"
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')  # Production only
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
